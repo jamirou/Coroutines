c7b7d4a31274539a5e906a1655b48f7413bf48d6
Jamiro <jamirocristian15@gmail.com>
2023-09-14 18:03:37 -0300

    added data model

---
+++ app/src/main/java/com/jamirodev/coroutinesapp/ItemsModel.kt
@@ -0,0 +1,6 @@
+package com.jamirodev.coroutinesapp
+
+data class ItemsModel(
+    val id: Int = 0,
+    val name: String = ""
+)
--- app/src/main/java/com/jamirodev/coroutinesapp/MainActivity.kt
+++ app/src/main/java/com/jamirodev/coroutinesapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -49,7 +50,11 @@ fun Content(viewModel: MainViewModel) {
     )
     {
         BtnColor()
-        Text(text = viewModel.resultState)
+        if (viewModel.isLoading) {
+            CircularProgressIndicator()
+        }else {
+            Text(text = viewModel.resultState)
+        }
         Button(onClick = { viewModel.fetchData() }) {
             Text(text = "Call API")
         }
